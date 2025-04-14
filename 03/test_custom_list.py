d5cf0659afb58be0b4d5fb66d3df05cc36c897ea
pikapyabort <pikapyabort@gmail.com>
2025-04-14 09:20:17 +0300

    hw2 second commit

--- 03/test_custom_list.py
+++ 03/test_custom_list.py
@@ -5,13 +5,13 @@ from custom_list import CustomList
 def test_initialization():
     obj_empty = CustomList()
     assert isinstance(obj_empty, CustomList)
-    assert obj_empty == CustomList([])
+    assert not list(obj_empty)
     obj_int = CustomList([1, 2, 3])
     assert isinstance(obj_int, CustomList)
-    assert obj_int == CustomList([1, 2, 3])
+    assert list(obj_int) == [1, 2, 3]
     obj_single = CustomList([-42])
     assert isinstance(obj_single, CustomList)
-    assert obj_single == CustomList([-42])
+    assert list(obj_single) == [-42]
 
 
 def test_add_custom_list():
@@ -21,9 +21,13 @@ def test_add_custom_list():
     obj2_copy = CustomList(obj2.copy())
     result1 = obj1 + obj2
     assert isinstance(result1, CustomList)
-    assert result1 == CustomList([6, 3, 10, 7])
-    assert obj1 == obj1_copy
-    assert obj2 == obj2_copy
+    assert list(result1) == [6, 3, 10, 7]
+    assert list(obj1) == list(obj1_copy)
+    assert list(obj2) == list(obj2_copy)
+    obj3 = CustomList([10, 20])
+    obj4 = CustomList([1, 2, 3, 4])
+    result2 = obj3 + obj4
+    assert list(result2) == [11, 22, 3, 4]
 
 
 def test_add_list():
@@ -34,11 +38,17 @@ def test_add_list():
     result1 = obj + lst
     result2 = lst + obj
     assert isinstance(result1, CustomList)
-    assert result1 == CustomList([12, 5])
+    assert list(result1) == [12, 5]
     assert isinstance(result2, CustomList)
-    assert result2 == CustomList([12, 5])
-    assert obj == obj_copy
+    assert list(result2) == [12, 5]
+    assert list(obj) == list(obj_copy)
     assert lst == lst_copy
+    obj2 = CustomList([1, 2, 3])
+    lst2 = [10, 20]
+    result3 = obj2 + lst2
+    result4 = lst2 + obj2
+    assert list(result3) == [11, 22, 3]
+    assert list(result4) == [11, 22, 3]
 
 
 def test_add_int():
@@ -47,10 +57,10 @@ def test_add_int():
     result1 = obj + 10
     result2 = 10 + obj
     assert isinstance(result1, CustomList)
-    assert result1 == CustomList([12, 15])
+    assert list(result1) == [12, 15]
     assert isinstance(result2, CustomList)
-    assert result2 == CustomList([12, 15])
-    assert obj == obj_copy
+    assert list(result2) == [12, 15]
+    assert list(obj) == list(obj_copy)
 
 
 def test_sub_custom_list():
@@ -60,9 +70,13 @@ def test_sub_custom_list():
     obj2_copy = CustomList(obj2.copy())
     result = obj1 - obj2
     assert isinstance(result, CustomList)
-    assert result == CustomList([4, -1, -4, 7])
-    assert obj1 == obj1_copy
-    assert obj2 == obj2_copy
+    assert list(result) == [4, -1, -4, 7]
+    assert list(obj1) == list(obj1_copy)
+    assert list(obj2) == list(obj2_copy)
+    obj3 = CustomList([10, 5])
+    obj4 = CustomList([2, 2, 2, 2])
+    result2 = obj3 - obj4
+    assert list(result2) == [8, 3, -2, -2]
 
 
 def test_sub_list():
@@ -73,11 +87,17 @@ def test_sub_list():
     result1 = obj - lst
     result2 = lst - obj
     assert isinstance(result1, CustomList)
-    assert result1 == CustomList([8, -5])
+    assert list(result1) == [8, -5]
     assert isinstance(result2, CustomList)
-    assert result2 == CustomList([-8, 5])
-    assert obj == obj_copy
+    assert list(result2) == [-8, 5]
+    assert list(obj) == list(obj_copy)
     assert lst == lst_copy
+    obj2 = CustomList([10, 20, 30])
+    lst2 = [1, 1]
+    result3 = obj2 - lst2
+    result4 = lst2 - obj2
+    assert list(result3) == [9, 19, 30]
+    assert list(result4) == [-9, -19, -30]
 
 
 def test_sub_int():
@@ -86,10 +106,10 @@ def test_sub_int():
     result1 = obj - 10
     result2 = 10 - obj
     assert isinstance(result1, CustomList)
-    assert result1 == CustomList([-8, -5])
+    assert list(result1) == [-8, -5]
     assert isinstance(result2, CustomList)
-    assert result2 == CustomList([8, 5])
-    assert obj == obj_copy
+    assert list(result2) == [8, 5]
+    assert list(obj) == list(obj_copy)
 
 
 def test_eq():
@@ -132,15 +152,15 @@ def test_empty_list():
     result4 = obj_emp + 5
     result5 = obj_emp + obj_emp
     assert isinstance(result1, CustomList)
-    assert result1 == CustomList([6, 6, 6])
+    assert list(result1) == [6, 6, 6]
     assert isinstance(result2, CustomList)
-    assert result2 == CustomList([-6, -6, -6])
+    assert list(result2) == [-6, -6, -6]
     assert isinstance(result3, CustomList)
-    assert result3 == CustomList([6, 6, 6])
+    assert list(result3) == [6, 6, 6]
     assert isinstance(result4, CustomList)
-    assert result4 == CustomList([])
+    assert not list(result4)
     assert isinstance(result5, CustomList)
-    assert result5 == CustomList([])
+    assert not list(result5)
 
 
 def test_invalid_operations():
